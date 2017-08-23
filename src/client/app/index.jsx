d0bef5a78efaf984584c3d2bd7272ac1115110a9
Owen Cliffe <owen.cliffe@oracle.com>
2017-08-23 01:54:14 +0100

    Removing node modules

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,13 +1,31 @@
+require('file-loader?name=[name].[ext]!./index.html');
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
+import Timeline from 'react-visjs-timeline'
+const options = {
+    width: '100%',
+    height: '60px',
+    stack: false,
+    showMajorLabels: true,
+    showCurrentTime: true,
+    zoomMin: 1000000,
+    type: 'background',
+    format: {
+        minorLabels: {
+            minute: 'h:mma',
+            hour: 'ha'
+        }
+    }
+}
 
 class App extends React.Component {
+
+
   render () {
   return (
     <div>
       <p> Hello React!</p>
-      <AwesomeComponent />
+      <Timeline options={options}/>
     </div>
   );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,36 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var BUILD_DIR = path.resolve(__dirname, 'dist');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
-      }
-    ]
-  }
-};
+        entry: APP_DIR + '/index.jsx',
+
+    output: {
+            path: BUILD_DIR,
+            filename: 'bundle.js'
+        },
+        module: {
+            loaders: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }, {
+                test: /\.jsx?/,
+                include:
+                APP_DIR,
+                use: 'babel-loader'
+            },
+                {
+                    test: /\.(jpe?g|png|gif|svg)$/i,
+                    loaders: [
+                        'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+                        'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.iinterlaced=false'
+                    ]
+                }
+            ]
+        }
+    }
+;
 
 module.exports = config;
