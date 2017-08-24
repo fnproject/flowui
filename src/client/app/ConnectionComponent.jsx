af3310419947250fb2de3f70c2b4e9e6bfe4e323
Rae Jeffries-Harris <rae.jeffries.harris@oracle.com>
2017-08-24 12:06:04 +0100

    Works when completer is giving all events graphId

--- src/client/app/ConnectionComponent.jsx
+++ src/client/app/ConnectionComponent.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 
-function connectWs(component) {
-  var map = new Map();
+var map = {};
 
+
+function createValue(data) {
+  const ts = ((data['data'])['ts']);
+  const type = data['type'];
+
+  var value = new Object();
+  value.type = type;
+  value.ts = ts;
+
+  return value;
+}
+
+function manageEvent(event, ws, component) {
+
+  console.log(event.data);
+    const data = JSON.parse(event.data);
+    console.log("Got new state", data);
+    const graphId = (data['data'])['graphId'];
+    console.log("Graph Id: ", graphId);
+    ws.send('{"command": "subscribe","graph_id" : "' + graphId + '"}');
+    if(!(graphId in map)){
+      component.setState({graphs : graphId + ", " + component.state.graphs});
+      map[graphId] = [];
+    }
+    var values = map[graphId];
+    values.push(createValue(data));
+    map[graphId] = values;
+    console.log(map[graphId]);
+    component.setState({events : event.data + component.state.events});
+}
+
+function connectWs(component) {
     let wsUrl = "ws://localhost:8081/wss";
 
     console.log("connecting to ws: ", wsUrl);
 
     const ws = new WebSocket(wsUrl);
     ws.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        console.log("Got new state", data);
-        if(data.type === 'model.GraphCreatedEvent'){
-          const graphId = (data['data'])['graphId'];
-          console.log("Graph Id: ", graphId);
-          ws.send('{"command": "subscribe","graph_id" : "' + graphId + '"}');
-          component.setState({graphs : event.data + component.state.graphs});
-        } else {
-          component.setState({events : event.data + component.state.events});
-        }
+      manageEvent(event, ws, component);
     };
 }
 
@@ -38,8 +60,20 @@ class ConnectionComponent extends React.Component {
     render() {
       return (
         <div>
-          <p>Graphs : <strong>{this.state.graphs}</strong></p>
-        <p>Events : <strong>{this.state.events}</strong></p>
+          { Object.keys(map).map(function (key) {
+            return (
+              <div>
+                <p>
+                  Graph: {key}
+                  Events: {map[key].map(function (listElem){
+                      return (
+                        <div> {listElem.type} </div>
+                      );
+                    })}
+                  </p>
+              </div>
+              );
+          }, this)}
       </div>
       );
     }
