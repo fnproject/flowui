5ae31f8ac5cf2eba54073018406fa3b88a4168ff
Owen Cliffe <owen.cliffe@oracle.com>
2017-09-14 04:47:30 +0100

    sorting out styles and web socket proxies

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ var app = express();
 
 var completerUrl = process.env['COMPLETER_BASE_URL'] || 'http://localhost:8081';
 var fnUrl = process.env['API_URL'] || 'http://localhost:8080';
-
+let completerProxy = proxy(completerUrl, {ws: true, changeOrigin:true,pathRewrite: {"^/completer": ""}});
+app.use('/completer', completerProxy);
 app.use('/fn', proxy(fnUrl, {pathRewrite: {"^/fn": ""}}));
 
-app.use('/completer', proxy(completerUrl, {ws: true, pathRewrite: {"^/completer": ""}}));
 app.use(express.static('dist'));
 
 var port = process.env['PORT'] || 3000
+var server = app.listen(port);
+
+server.on('upgrade', completerProxy.upgrade);  // <-- subscribe to http 'upgrade'
+
 console.log("listening on " + port);
-app.listen(port);
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,60 @@
 var webpack = require('webpack');
 var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'dist');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 const debug = true
 
 var config = {
-        entry: APP_DIR + '/index.jsx',
-        devtool: debug ? '#source-map' : false,
+    entry: APP_DIR + '/index.jsx',
+    devtool: debug ? '#source-map' : false,
 
     output: {
-            path: BUILD_DIR,
-            filename: 'bundle.js'
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader?modules'],
+        }, {
+            test: /\.jsx?/,
+            include:
+            APP_DIR,
+            use: 'babel-loader'
         },
-        module: {
-            loaders: [{
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader?modules'],
-            }, {
-                test: /\.jsx?/,
-                include:
-                APP_DIR,
-                use: 'babel-loader'
-            },
-                {
-                    test: /\.(jpe?g|png|gif|svg)$/i,
-                    loaders: [
-                        'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-                        'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.iinterlaced=false'
-                    ]
-                }
-            ]
-        },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.iinterlaced=false'
+                ]
+            }
+        ]
+    },
 
     devServer: {
         /* Send API requests on localhost to API server get around CORS */
         proxy: {
             '/fn': {
-                target:'http://localhost:8080/',
-                pathRewrite: {"^/fn" : ""}
+                target: 'http://localhost:8080/',
+                pathRewrite: {"^/fn": ""}
 
             },
             '/completer': {
-                target:'http://localhost:8081/',
-                pathRewrite: {"^/completer" : ""}
+                target: 'http://localhost:8081/',
+                pathRewrite: {"^/completer": ""}
 
             },
         }
-    }
-    }
-;
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            // {output}/file.txt
+            {from: 'src/client/app/background.png'},
+            {from: 'src/client/app/app.css'},
+        ])]
+};
 
 module.exports = config;
