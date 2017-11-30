f478dd814d9084808bdd01c2656fb1d846e82eb3
gerardo_viedma <gerardo.viedma.nunez@oracle.com>
2017-11-30 16:41:11 +0000

    splits newline delimited json events from stream

--- src/client/app/completerStreamClient.js
+++ src/client/app/completerStreamClient.js
@@ -8,6 +8,7 @@ class CompleterStreamClient {
 
     connect() {
         let url = "/completer/v1/stream";
+        //let url = `/completer/v1/flows/cbb714b8-0e7e-450d-b53a-2a0b98226118/stream`
         var target = this.receiver;
         this.subscribeStream(url, true, (msg)=>{
             this.receiver(msg);
@@ -36,22 +37,28 @@ class CompleterStreamClient {
                 }
 
                 bytesReceived += result.value.length;
-
                 console.log("Got " + bytesReceived + "  bytes")
 
-                let msg = decoder.decode(result.value);
-                console.log("Got message : " + msg);
+                let chunk = decoder.decode(result.value);
+                // newline delimited JSON events
+                var msgs = chunk.split('\n');
+                console.log(`Received ${msgs.length - 1} events in cunk`);
 
-                let jsonMessage = JSON.parse(msg);
-                jsonMessage.result.is_lifecycle = isLifecycleStream;
-
-                callback(jsonMessage.result);
+                for (var i = 0; i < msgs.length - 1; i++) {
+                    console.log("Processing msg", msgs[i]);                    
+                    let jsonMessage = JSON.parse(msgs[i]);
+                    jsonMessage.result.is_lifecycle = isLifecycleStream;
+                    callback(jsonMessage.result);
+                }
                 return reader.read().then(processResult);
             });
-        });
+        })
+        .catch(this.onChunkedResponseError);
     }
 
-
+    onChunkedResponseError(err) {
+        console.error("Error processing chunk", err);
+    }
 
 }
 
