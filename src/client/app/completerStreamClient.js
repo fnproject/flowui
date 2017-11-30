57c1467ca7c5a25f6a635aaecab5ff0bd6e63520
gerardo_viedma <gerardo.viedma.nunez@oracle.com>
2017-11-30 17:11:56 +0000

    adds oboe dependency for consuming json streams

--- src/client/app/completerStreamClient.js
+++ src/client/app/completerStreamClient.js
@@ -1,3 +1,5 @@
+let oboe = require("oboe/dist/oboe-browser.min");
+
 class CompleterStreamClient {
 
     constructor() {
@@ -8,7 +10,6 @@ class CompleterStreamClient {
 
     connect() {
         let url = "/completer/v1/stream";
-        //let url = `/completer/v1/flows/cbb714b8-0e7e-450d-b53a-2a0b98226118/stream`
         var target = this.receiver;
         this.subscribeStream(url, true, (msg)=>{
             this.receiver(msg);
@@ -23,37 +24,14 @@ class CompleterStreamClient {
         });
     }
 
+
     subscribeStream(url, isLifecycleStream, callback) {
-        console.log("Subscribing to stream on", url);        
-        fetch(url, {method: 'GET', mode: 'no-cors'}).then(response => {
-            let reader = response.body.getReader();
-
-            let decoder = new TextDecoder('utf-8')
-            let bytesReceived = 0;
-            return reader.read().then(function processResult(result) {
-                if (result.done) {
-                    console.log("stream closed");
-                    return;
-                }
-
-                bytesReceived += result.value.length;
-                console.log("Got " + bytesReceived + "  bytes")
-
-                let chunk = decoder.decode(result.value);
-                // newline delimited JSON events
-                var msgs = chunk.split('\n');
-                console.log(`Received ${msgs.length - 1} events in cunk`);
-
-                for (var i = 0; i < msgs.length - 1; i++) {
-                    console.log("Processing msg", msgs[i]);                    
-                    let jsonMessage = JSON.parse(msgs[i]);
-                    jsonMessage.result.is_lifecycle = isLifecycleStream;
-                    callback(jsonMessage.result);
-                }
-                return reader.read().then(processResult);
+        oboe(url)
+            .done((data) => {
+                console.log("Processing msg", data);
+                data.result.is_lifecycle = isLifecycleStream;
+                callback(data.result);
             });
-        })
-        .catch(this.onChunkedResponseError);
     }
 
     onChunkedResponseError(err) {
